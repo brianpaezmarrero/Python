1c08aa0af2883cded7959110b51a6a26f35d202f
brianpaezmarrero <85861662+brianpaezmarrero@users.noreply.github.com>
2021-07-15 22:46:18 -0700

    Add files via upload

--- flask/hello_flask/server.py
+++ flask/hello_flask/server.py
@@ -1,32 +1,21 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from typing_extensions import IntVar
+from flask import Flask 
 
-
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-# Return the string 'Hello World!' as a response
+app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return "Hello World"
-
-
-@app.route('/dojo')
-def dojo():
-    return "Dojo"
-
+    return 'Hello World'
 
 @app.route('/<string:say>/<string:name>')
-def michael(say,name):
-    return f"{say}{name}"
+def name(say,name):
+    return f"{say} {name}"
+
+@app.route('/repeat/<string:hello>/<int:num>')
+def repeat(hello,num):
+    return f"{hello * num}"
 
 
-@app.route('/repeat/<int:num>/<string:hello>')
-def repeat(num,hello):
-    for i in range(num):
-        i * print(hello)
-
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
-
-
-
+if __name__=='__main__':
+    app.run(debug=True)
--- flask/playground/server.py
+++ flask/playground/server.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,7 +7,7 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/<int:num>')
+@app.route('/<int:num>/<color>')
 def add_box(num):
     return render_template("box.html",num=num)
 
